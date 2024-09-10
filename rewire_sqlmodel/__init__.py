457fb8843af9a89301554e03ef19cd8a091ffc88
Ivan Vozhakov <gou177@bk.ru>
2024-09-10 10:14:34 +0500

    feat: Convenience functions for sql selects

--- rewire_sqlmodel/__init__.py
+++ rewire_sqlmodel/__init__.py
@@ -210,7 +210,8 @@ if PluginConfig.patch_types:
                 ]
                 if len(args) == 1:
                     root_type = args[0]
-                    field.nullable = True  # type: ignore
+                    if hasattr(field, "nullable"):
+                        field.nullable = True  # type: ignore
             if get_origin(root_type) == Annotated:
                 root_type = get_args(root_type)[0]
 
@@ -351,39 +352,60 @@ class LazySession:
 class SelectOfScalarExtended[TM](SelectOfScalar[TM]):
     inherit_cache = True
 
-    def __await__(self):
+    def __await__(self) -> typing.Generator[Any, Any, ScalarResult[TM]]:
         return self.exec().__await__()
 
     async def exec(self) -> ScalarResult[TM]:
         session = session_context.get()
         return await session.exec(self)  # type: ignore
 
-    async def stream(self):
+    async def stream(self) -> typing.Sequence[TM]:
         return (await self).all()
 
-    async def all(self):
+    async def all(self) -> typing.Sequence[TM]:
         return (await self).all()
 
-    async def unique(self, strategy: Any | None = None):
+    async def all_map[T](self, callback: Callable[[TM], T]):
+        return list(map(callback, await self.all()))
+
+    async def unique(self, strategy: Any | None = None) -> ScalarResult[TM]:
         return (await self).unique(strategy)
 
-    async def partitions(self, size: int | None = None):
+    async def partitions(self, size: int | None = None) -> typing.Iterator[typing.Sequence[TM]]:
         return (await self).partitions(size)
 
-    async def fetchall(self):
+    async def fetchall(self) -> typing.Sequence[TM]:
         return (await self).fetchall()
 
-    async def fetchmany(self, size: int | None = None):
+    async def fetchmany(self, size: int | None = None) -> typing.Sequence[TM]:
         return (await self).fetchmany(size)
 
-    async def first(self):
+    async def first(self) -> TM | NoneType:
         return (await self).first()
 
-    async def one(self):
+    async def one(self) -> TM:
         return (await self).one()
 
-    async def one_or_none(self):
+    async def one_or_none(self) -> TM | NoneType:
         return (await self).one_or_none()
+
+    async def one_or_raise(self, exception: BaseException) -> TM:
+        result = await self.one_or_none()
+        if result is None:
+            raise exception
+        return result
+
+    async def one_or_call[T](self, callback: Callable[[], T]) -> T | TM:
+        result = await self.one_or_none()
+        if result is None:
+            return callback()
+        return result
+
+    async def one_or[T](self, default: T) -> T | TM:
+        result = await self.one_or_none()
+        if result is None:
+            return default
+        return result
 
 
 def fk(column: Any):
